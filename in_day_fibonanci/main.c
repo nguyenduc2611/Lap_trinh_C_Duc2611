957dd0b5a2d552396710246f8da791f9a88940ab
nguyenduc2611 <nguyenvanduc2611@gmail.com>
2017-11-01 00:25:24 +0700

    xong_fibanacci

---
+++ in_day_fibonanci/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int n;
+	int j =0;
+	int i=1;
+	int* p;
+	p = (int *) calloc(100, sizeof(int));
+	printf("nhap so n: ");
+	scanf("%d", &n);
+	*p =*(p+1)= 1;
+	while (*(p+i)<n)
+	{
+		*(p+i+1)=*(p+i)+*(p+i-1);
+		i++;
+	}
+	for (j=0;j<i;j++)
+	{
+		printf("\n %d", p[j]);
+	}
+	free (p);
+	return 0;
+}
